4fccb868946c00c89d8f436d4589e8127c468835
Dale Georg <dale.georg@clearml.ai>
2026-01-27 15:18:23 -0500

    Add Python project I don't have installed.

--- remote/test.py
+++ remote/test.py
@@ -1,6 +1,10 @@
 import os
 import socket
 
+# import something I don't have
+import skyfield
+
+
 # Print all environment variables
 for key, value in os.environ.items():
     print(f"{key}={value}")
--- remote/test_remote.py
+++ remote/test_remote.py
@@ -4,9 +4,9 @@ from clearml import Task
 task = Task.init("Dale's Playground", 'Some Task')
 
 task.set_script(
-    repository='https://github.com/dalegeorg-clearml',
-    branch='master',
-    working_dir='testing',
+    repository='https://github.com/dalegeorg-clearml/testing',
+    branch='main',
+    working_dir='remote',
     entry_point='test.py')
 
 task.execute_remotely("dale")
